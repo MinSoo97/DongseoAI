4962b3b8d1f92b7ec964ef79db579001986cef36
hubblin <43435547+hubblin@users.noreply.github.com>
2020-08-26 09:25:33 +0900

    add Menu & Modify package

--- hackathon2020/app/src/main/java/com/example/hackathon2020/Multiple_choice.java
+++ hackathon2020/app/src/main/java/com/example/hackathon2020/Multiple_choice.java
@@ -1,4 +1,4 @@
-package com.example.prototype;
+package com.example.hackathon2020;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
--- hackathon2020/app/src/main/java/com/example/hackathon2020/Problem_result_activity.java
+++ hackathon2020/app/src/main/java/com/example/hackathon2020/Problem_result_activity.java
@@ -1,4 +1,4 @@
-package com.example.prototype;
+package com.example.hackathon2020;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
